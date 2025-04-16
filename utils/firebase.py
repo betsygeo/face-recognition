fc63bb54129dfb233ec7964c59a8189b7ec78b74
Betsy George <betsygeo34@gmail.com>
2025-04-16 12:31:39 -0400

    env

--- utils/firebase.py
+++ utils/firebase.py
@@ -1,5 +1,3 @@
-import base64
-import json
 from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, storage, firestore
@@ -7,10 +5,8 @@ import os
 
 load_dotenv('.env.local')
 
-cred_json = base64.b64decode(os.environ["FIREBASE_CREDENTIALS"]).decode("utf-8")
-cred_dict = json.loads(cred_json)
 
-cred = credentials.Certificate(cred_dict)
+cred = credentials.Certificate("firebase-adminsdk.json")
 firebase_admin.initialize_app(cred, {
     'storageBucket': os.getenv("FIREBASE_STORAGE_BUCKET")
 })
